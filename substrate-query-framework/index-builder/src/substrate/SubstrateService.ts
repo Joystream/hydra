76266e340e0430aee506e0ca6dff297ba2020ccd
dzhelezov <dzhelezov@gmail.com>
2020-09-09 12:00:40 +0300

    update polkadot version and remove metadata updates

--- substrate-query-framework/index-builder/src/substrate/SubstrateService.ts
+++ substrate-query-framework/index-builder/src/substrate/SubstrateService.ts
@@ -2,7 +2,6 @@ import { Hash, Header, BlockNumber, EventRecord, SignedBlock } from '@polkadot/t
 import { Callback, Codec } from '@polkadot/types/types';
 import { u32 } from '@polkadot/types/primitive';
 import { ApiPromise } from '@polkadot/api';
-import { getSpecTypes } from '@polkadot/types-known';
 
 import ISubstrateService from './ISubstrateService';
 import { UnsubscribePromise } from '@polkadot/api/types';
@@ -24,46 +23,8 @@ export class SubstrateService implements ISubstrateService {
     this._specVersion = api.createType('u32', this._versionReset);
   }
 
-  /**
-   * Update api metadata to the latest
-   */
-  async resetMeta(): Promise<ApiPromise> {
-    return await this.ensureMeta(await this.getFinalizedHead());
-  }
-
-  /**
-   * Makes sure the api has correct types and metadata before fetching the block data
-   * @param blockHash Hash | Uint8Array | string
-   */
-  async ensureMeta(blockHash: Hash | Uint8Array | string): Promise<ApiPromise> {
-    const api = this._api;
-
-    try {
-      const version = await api.rpc.state.getRuntimeVersion(blockHash);
-      const blockSpecVersion = version.specVersion;
-
-      // Register types for the block and update metadata if spec version is diffrent
-      if (!this._specVersion.eq(blockSpecVersion)) {
-        this._specVersion = blockSpecVersion;
-
-        const meta = await api.rpc.state.getMetadata(blockHash);
-        const chain = await api.rpc.system.chain();
-
-        api.registerTypes(getSpecTypes(api.registry, chain, version.specName, blockSpecVersion));
-        api.registry.setMetadata(meta);
-      }
-    } catch (error) {
-      console.error(`Failed to get Metadata for block ${JSON.stringify(blockHash, null, 2)}, using latest.`);
-      console.error(error);
-      this._specVersion = api.createType('u32', this._versionReset);
-    }
-
-    return api;
-  }
-
   async getHeader(hash: Hash | Uint8Array | string): Promise<Header> {
-    const api = await this.ensureMeta(hash);
-    return api.rpc.chain.getHeader(hash);
+    return this._api.rpc.chain.getHeader(hash);
   }
 
   getFinalizedHead(): Promise<Hash> {
@@ -81,13 +42,11 @@ export class SubstrateService implements ISubstrateService {
 
   async getBlock(hash: Hash | Uint8Array | string): Promise<SignedBlock> {
     debug(`Fething block: BlockHash: ${JSON.stringify(hash)}`)
-    const api = await this.ensureMeta(hash);
-    return api.rpc.chain.getBlock(hash);
+    return this._api.rpc.chain.getBlock(hash);
   }
 
   async eventsAt(hash: Hash | Uint8Array | string): Promise<EventRecord[] & Codec> {
     debug(`Fething events. BlockHash:  ${JSON.stringify(hash)}`)
-    const api = await this.ensureMeta(hash);
-    return api.query.system.events.at(hash);
+    return this._api.query.system.events.at(hash);
   }
 }
