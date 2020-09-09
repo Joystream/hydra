95efdd1160594bb9b789cc4e218e89609dcd5814
dzhelezov <dzhelezov@gmail.com>
2020-09-09 12:19:03 +0300

    move waitFor wrappers to API calls

--- substrate-query-framework/index-builder/src/substrate/SubstrateService.ts
+++ substrate-query-framework/index-builder/src/substrate/SubstrateService.ts
@@ -6,8 +6,13 @@ import { ApiPromise } from '@polkadot/api';
 import ISubstrateService from './ISubstrateService';
 import { UnsubscribePromise } from '@polkadot/api/types';
 import Debug from 'debug';
+import { numberEnv } from '../utils/env-flags';
+import { withTimeout } from '../utils/wait-for';
 
 const debug = Debug('index-builder:producer');
+
+const SUBSTRATE_API_TIMEOUT = numberEnv('SUBSTRATE_API_TIMEOUT') || 5000;
+
 
 export class SubstrateService implements ISubstrateService {
   // Enough large number
@@ -24,29 +29,33 @@ export class SubstrateService implements ISubstrateService {
   }
 
   async getHeader(hash: Hash | Uint8Array | string): Promise<Header> {
-    return this._api.rpc.chain.getHeader(hash);
+    return this._withTimeout(this._api.rpc.chain.getHeader(hash));
   }
 
   getFinalizedHead(): Promise<Hash> {
-    return this._api.rpc.chain.getFinalizedHead();
+    return this._withTimeout(this._api.rpc.chain.getFinalizedHead());
   }
 
   subscribeNewHeads(v: Callback<Header>): UnsubscribePromise {
-    return this._api.rpc.chain.subscribeNewHeads(v);
+    return this._withTimeout(this._api.rpc.chain.subscribeNewHeads(v));
   }
 
   async getBlockHash(blockNumber?: BlockNumber | Uint8Array | number | string): Promise<Hash> {
     debug(`Fetching block hash: BlockNumber: ${JSON.stringify(blockNumber)}`)
-    return this._api.rpc.chain.getBlockHash(blockNumber);
+    return this._withTimeout(this._api.rpc.chain.getBlockHash(blockNumber));
   }
 
   async getBlock(hash: Hash | Uint8Array | string): Promise<SignedBlock> {
     debug(`Fething block: BlockHash: ${JSON.stringify(hash)}`)
-    return this._api.rpc.chain.getBlock(hash);
+    return this._withTimeout(this._api.rpc.chain.getBlock(hash));
   }
 
   async eventsAt(hash: Hash | Uint8Array | string): Promise<EventRecord[] & Codec> {
     debug(`Fething events. BlockHash:  ${JSON.stringify(hash)}`)
-    return this._api.query.system.events.at(hash);
+    return this._withTimeout(this._api.query.system.events.at(hash));
+  }
+
+  private async _withTimeout<T>(promiseFn: Promise<T>): Promise<T> {
+    return withTimeout(promiseFn, `Timed out: ${JSON.stringify(promiseFn, null, 2)}`, SUBSTRATE_API_TIMEOUT);
   }
 }
