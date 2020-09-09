4e2e8a5906f21cca0b8bedb9e5506cf6cf4c4e3d
dzhelezov <dzhelezov@gmail.com>
2020-09-09 20:42:35 +0300

    allow additional entities to config

--- substrate-query-framework/index-builder/src/db/helper.ts
+++ substrate-query-framework/index-builder/src/db/helper.ts
@@ -29,10 +29,12 @@ export function fillRequiredWarthogFields<T>(entity: DeepPartial<T>): DeepPartia
   return Object.assign(entity, requiredFields);
 }
 
-export async function createDBConnection(): Promise<Connection> {
+export async function createDBConnection(entities: any[] = []): Promise<Connection> {
   //const connectionOptions = await getConnectionOptions();
-  debug(`DB config: ${JSON.stringify(config(), null, 2)}`);
-  return createConnection(config());
+  const _config = config();
+  entities.map((e) => _config.entities?.push(e));
+  debug(`DB config: ${JSON.stringify(_config, null, 2)}`);
+  return createConnection(_config);
 }
 
 
