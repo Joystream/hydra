1eb53d1a5432d2d9f348c4e4cf3b7e1a512dc05c
dzhelezov <dzhelezov@gmail.com>
2020-09-11 12:40:52 +0300

    add constant delay backoffstrategy

--- substrate-query-framework/index-builder/src/utils/BackOffStategy.ts
+++ substrate-query-framework/index-builder/src/utils/BackOffStategy.ts
@@ -18,7 +18,26 @@ export interface BackoffStrategy {
 const INITIAL_BACKOFF_TIME:number = numberEnv('DEFAULT_BACKOFF_TIME_MS') || 250;
 const MAX_BACKOFF_TIME: number = numberEnv('MAX_BACKOFF_TIME_MS') || 1000 * 60 * 5; // 5 mins
 
-export default class ExponentialBackOffStrategy implements BackoffStrategy {
+export class ConstantBackOffStrategy implements BackoffStrategy {
+  constructor (public readonly waitTime: number) {
+
+  }
+
+  public resetBackoffTime(): void {
+    // NOOP
+  }
+
+  public registerFailure(): void {
+    // NOOP
+  }
+
+  public getBackOffMs(): number {
+    return this.waitTime;
+  }
+
+}
+
+export class ExponentialBackOffStrategy implements BackoffStrategy {
   
   private backoffTime = INITIAL_BACKOFF_TIME;
   public maxBackOffTime = MAX_BACKOFF_TIME;
