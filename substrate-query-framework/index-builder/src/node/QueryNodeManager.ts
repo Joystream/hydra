6bb8e8f846352c3b398d260cdb9a39e867d7e07a
dzhelezov <dzhelezov@gmail.com>
2020-09-17 12:05:51 +0300

    pass additional entities to ProcessorOptions

--- substrate-query-framework/index-builder/src/node/QueryNodeManager.ts
+++ substrate-query-framework/index-builder/src/node/QueryNodeManager.ts
@@ -43,7 +43,8 @@ export class QueryNodeManager {
    * @param options options passed to create the mappings
    */
   async process(options: ProcessorOptions): Promise<void> {
-    await createDBConnection();
+    const extraEntities = options.entities ? options.entities : [];
+    await createDBConnection(extraEntities);
     
     const processor =  MappingsProcessor.create(options);
     await processor.start();
