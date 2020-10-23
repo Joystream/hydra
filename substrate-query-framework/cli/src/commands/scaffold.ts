434464f0d61ac2b6b88854282424d6fea1925e46
Dmit <dzhelezov@gmail.com>
2020-10-23 18:01:50 +0300

    new default kusama indexer endpoint

--- substrate-query-framework/cli/src/commands/scaffold.ts
+++ substrate-query-framework/cli/src/commands/scaffold.ts
@@ -10,6 +10,7 @@ import execa = require('execa');
 import glob = require('glob');
 
 const DEFAULT_WS_API_ENDPOINT = 'wss://kusama-rpc.polkadot.io/';
+const DEFAULT_KUSAMA_INDEXER = 'https://kusama-indexer.joystream.app/graphql';
 
 export default class Scaffold extends Command {
   static description = `Starter kit: generates a directory layout and a sample schema file`;
@@ -91,7 +92,7 @@ export default class Scaffold extends Command {
       return ctx;
     }
     const indexerUrl = (await cli.prompt('Provide an indexer GraphQL API endpoint to source events from', {
-      default: 'https://hakusama.joystream.app/graphql',
+      default: DEFAULT_KUSAMA_INDEXER,
     })) as string;
     ctx = { ...ctx, indexerUrl };
 
