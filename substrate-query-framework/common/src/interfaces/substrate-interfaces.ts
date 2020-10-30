c92ee49c469564529b12d29bb2294cba4f185fd9
Dmit <dzhelezov@gmail.com>
2020-10-30 15:06:32 +0300

    refactor: extract @dzlzv/hydra-common package

---
+++ substrate-query-framework/common/src/interfaces/substrate-interfaces.ts
@@ -0,0 +1,38 @@
+import { AnyJson, AnyJsonField } from './json-types'
+import * as BN from 'bn.js'
+
+export interface EventParam {
+  type: string
+  name: string
+  value: AnyJsonField
+}
+
+export interface ExtrinsicArg {
+  type: string
+  name: string
+  value: AnyJsonField
+}
+
+export interface SubstrateEvent {
+  name: string
+  method: string
+  section?: string
+  params: Array<EventParam>
+  index: number
+  id: string
+  blockNumber: number
+  extrinsic?: SubstrateExtrinsic
+}
+
+export interface SubstrateExtrinsic {
+  method: string
+  section: string
+  versionInfo?: string
+  meta?: AnyJson
+  era?: AnyJson
+  signer: string
+  args: ExtrinsicArg[]
+  signature?: string
+  hash?: string
+  tip: BN
+}
