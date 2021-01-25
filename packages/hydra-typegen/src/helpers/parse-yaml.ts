73a30da49dd6051029b3821a9744cff2ceee095a
Dmit <dzhelezov@gmail.com>
2021-01-25 19:44:21 +0300

    [hydra-typegen] and yml config support

---
+++ packages/hydra-typegen/src/helpers/parse-yaml.ts
@@ -0,0 +1,40 @@
+import YamlValidator from 'yaml-validator'
+import fs from 'fs'
+import YAML from 'yaml'
+
+import { error as logError } from '../log'
+import { IConfig } from '../commands/typegen'
+
+const options = {
+  structure: {
+    metadata: {
+      source: 'string',
+      'blockHash?': 'string',
+    },
+    'customTypes?': {
+      lib: 'string',
+      typedefs: 'string',
+    },
+    events: ['string'],
+    calls: ['string'],
+    outDir: 'string',
+    'strict?': 'boolean',
+  },
+  onWarning: function (error: unknown, filepath: unknown) {
+    logError(filepath + ' has error: ' + error)
+  },
+}
+
+export function parseConfigFile(location: string): IConfig {
+  const validator = new YamlValidator(options)
+  validator.validate([location])
+
+  if (validator.report()) {
+    throw new Error(
+      `Invalid config file at location ${location}: ${validator.logs.join(
+        '\n'
+      )}`
+    )
+  }
+  return YAML.parse(fs.readFileSync(location, 'utf8')) as IConfig
+}
