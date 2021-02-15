0e44906937ee011b7fda96b98a0d49b3fb88df2b
Dmit <dzhelezov@gmail.com>
2021-02-15 17:46:05 +0300

    [hydra-e2e-tests] increase timeout

--- packages/hydra-e2e-tests/run-tests.sh
+++ packages/hydra-e2e-tests/run-tests.sh
@@ -22,7 +22,7 @@ docker-compose up -d
 
 # wait for the processor to start grinding 
 attempt_counter=0
-max_attempts=10
+max_attempts=20
 
 until $(curl -s --head  --request GET http://localhost:3000/metrics/hydra_processor_last_scanned_block | grep "200" > /dev/null);  do
     if [ ${attempt_counter} -eq ${max_attempts} ];then
