a4ce62c58ca4305a3f2d50f407d9ed3d4dc5bc84
Dmit <dzhelezov@gmail.com>
2021-03-19 21:58:58 +0300

    refactor(hydra-processor): refactor IEventSource and GraphQLSource
    affects: @dzlzv/hydra-processor

--- packages/hydra-processor/src/ingest/index.ts
+++ packages/hydra-processor/src/ingest/index.ts
@@ -1,2 +1,13 @@
-export * from './GraphQLSource'
+import { GraphQLSource } from './GraphQLSource'
+import { IEventsSource } from './IEventsSource'
+
 export * from './IEventsSource'
+
+let eventSource: IEventsSource
+
+export const getEventSource: () => IEventsSource = () => {
+  if (!eventSource) {
+    eventSource = new GraphQLSource()
+  }
+  return eventSource
+}
