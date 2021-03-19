d3280d5e5ed37c74480af2de37c671b24003b59c
Dmit <dzhelezov@gmail.com>
2021-03-19 22:01:59 +0300

    refactor(hydra-processor): add new events and utility libs
    affects: @dzlzv/hydra-processor

--- packages/hydra-processor/src/start/config.ts
+++ packages/hydra-processor/src/start/config.ts
@@ -4,7 +4,7 @@ import { parseManifest, ProcessorManifest } from './manifest'
 import Debug from 'debug'
 import { setWarthogEnvs } from '../db/ormconfig'
 
-export const conf = cleanEnv(process.env, {
+const preConf = cleanEnv(process.env, {
   MANIFEST_PATH: str({ default: 'manifest.yml' }),
   INDEXER_ENDPOINT_URL: str({ devDefault: 'http://localhost:4001' }),
   NAME: str({ default: 'Hydra-Processor' }),
@@ -16,15 +16,28 @@ export const conf = cleanEnv(process.env, {
   BLOCK_WINDOW: num({ default: 100000 }),
   PROCESSOR_NAME: str({ default: 'hydra-processor' }),
   // Maximal number of events to process in a single transaction
-  BATCH_SIZE: num({ default: 10 }),
+  BATCH_SIZE: num({
+    default: 100,
+  }),
+
+  MAPPINGS_FACTOR: num({ default: 1 }),
+  QUEUE_FACTOR: num({ default: 2 }),
+  QUEUE_MAX_CAP_FACTOR: num({ default: 5 }),
   // Interval at which the processor pulls new blocks from the database
   // The interval is reasonably large by default. The trade-off is the latency
   // between the updates and the load to the database
   // It will be replaced by a poll-free subscription in the future
-  POLL_INTERVAL_MS: num({ default: 60 * 1000 }),
+  POLL_INTERVAL_MS: num({ default: 3 * 1000 }),
   // Wait for the indexer head block to be ahead for at least that number of blocks
   MIN_BLOCKS_AHEAD: num({ default: 0 }),
 })
+
+export const conf = {
+  ...preConf,
+  MAPPINGS_BATCH_SIZE: preConf.BATCH_SIZE * preConf.MAPPINGS_FACTOR,
+  QUEUE_BATCH_SIZE: preConf.BATCH_SIZE * preConf.QUEUE_FACTOR,
+  EVENT_QUEUE_MAX_CAPACITY: preConf.BATCH_SIZE * preConf.QUEUE_MAX_CAP_FACTOR,
+}
 
 let manifest: ProcessorManifest | undefined
 
