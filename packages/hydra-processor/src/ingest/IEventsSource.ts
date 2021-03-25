62456330d14386aa32d02313ec12452683771260
Dmit <dzhelezov@gmail.com>
2021-03-25 17:08:17 +0300

    feat(hydra-processor): implement GraphQL query builder
    affects: @dzlzv/hydra-processor

--- packages/hydra-processor/src/ingest/IEventsSource.ts
+++ packages/hydra-processor/src/ingest/IEventsSource.ts
@@ -3,13 +3,43 @@ import { SubstrateEvent } from '@dzlzv/hydra-common'
 import { IndexerStatus } from '../state'
 
 export interface Filter {
-  in: string[]
+  in: string[] | number[]
   gte: number | string
   lte: number | string
   gt: number | string
   lt: number | string
+  startsWith: string
+  contains: string
+  endsWith: string
 }
 
+export type QueryWhere<T> = Partial<{ [P in keyof T]: Partial<Filter> }>
+
+export interface QueryFilter<T> {
+  where: QueryWhere<T>
+  limit?: number
+  orderBy?: Partial<{
+    asc: string
+    desc: string
+  }>
+}
+
+// extract T from type of Array<T>
+export type ArrayElement<
+  ArrayType extends readonly unknown[]
+> = ArrayType extends readonly (infer ElementType)[] ? ElementType : never
+
+export type QueryField<T> =
+  | keyof T
+  | Partial<
+      {
+        [P in keyof T]: QueryFields<
+          T[P] extends readonly unknown[] ? ArrayElement<T[P]> : T[P]
+        >
+      }
+    >
+
+export type QueryFields<T> = Array<QueryField<T>>
 /**
  * Query for fetching events
  *  - strictly after event with IDs > afterID (if present)
@@ -22,6 +52,12 @@ export interface IndexerQuery {
   extrinsic?: Partial<Filter>
   block: Partial<Filter>
   limit?: number
+}
+
+export interface GraphQLQuery<T> {
+  name: string
+  query: QueryFilter<T>
+  fields: QueryFields<T>
 }
 
 export interface IEventsSource {
---
+++ packages/hydra-processor/src/ingest/graphql-query-builder.ts
@@ -0,0 +1,109 @@
+import { compact } from 'lodash'
+import { GraphQLQuery, QueryField, QueryFields, QueryWhere } from '.'
+import { format, stripSpaces } from '../util/utils'
+
+export type FilterValue = undefined | string | number | string[] | number[]
+
+export function isArray(f: FilterValue) {
+  return f !== undefined && (f as any).pop !== undefined
+}
+
+export function isString(f: string | number): f is string {
+  return (f as any).length !== undefined
+}
+
+export function formatScalar(value: string | number): string {
+  return isString(value) ? `"${value}"` : `${value}`
+}
+
+export function formatClause(name: string, value: FilterValue) {
+  if (value === undefined) return ''
+  return `${name}: ${formatValue(value)}`
+}
+
+export function formatValue(value: FilterValue): string {
+  if (value === undefined) {
+    return ''
+  }
+  if (isArray(value)) {
+    return `[${(value as string[])
+      .map((s: string | number) => `${formatScalar(s)}`)
+      .join(', ')}]`
+  }
+
+  return formatScalar(value as string | number)
+}
+
+export function clauses(
+  field: string,
+  filter: Record<string, FilterValue> | undefined
+): string[] {
+  if (filter === undefined) {
+    return ['']
+  }
+
+  return Object.keys(filter).reduce((acc, clause: string) => {
+    if (filter[clause] === undefined) {
+      return acc
+    }
+
+    acc.push(
+      `${field}_${clause}: ${formatValue(filter[clause] as FilterValue)}`
+    )
+    return acc
+  }, [] as string[])
+}
+
+export function buildWhere<T>(where: QueryWhere<T>): string {
+  let whereBody: string[] = Object.keys(where).reduce((acc, f) => {
+    acc.push(...clauses(f, where[f as keyof typeof where]))
+
+    return acc
+  }, [] as string[])
+  whereBody = compact(whereBody)
+  return stripSpaces(`where: { ${whereBody.join(', ')} }`)
+}
+
+export function buildFields<T>(fields: QueryFields<T>): string {
+  let output = ''
+  for (const field of fields) {
+    if (typeof field === 'string') {
+      // simple field
+      output = `${output}${field}\n`
+    } else {
+      // only a single key
+      const keys = Object.keys(field)
+      if (keys.length > 1) {
+        throw new Error(
+          'Composite field must have a single key, split into multiple composit fields if necessary'
+        )
+      }
+      const key = keys[0] as keyof T
+      const nestedFields = (field as Partial<
+        { [P in keyof T]: QueryFields<T[P]> }
+      >)[key] as QueryFields<T[typeof key]>
+
+      output = `
+      ${output}${key} {
+        ${buildFields(nestedFields)}
+      }\n`
+    }
+  }
+  return format(output)
+}
+
+export function buildQuery<T>({
+  name,
+  query: { where, limit, orderBy },
+  fields,
+}: GraphQLQuery<T>): string {
+  const parts: string[] = compact([
+    buildWhere(where),
+    formatClause('limit', limit),
+    ...clauses('orderBy', orderBy as Record<string, FilterValue> | undefined),
+  ])
+
+  return stripSpaces(`${name}( ${parts.join(', ')} ) {
+    ${buildFields(fields)}
+  }`)
+}
