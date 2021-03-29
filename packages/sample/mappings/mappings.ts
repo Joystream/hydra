4d009ffdd338479184f2c9286ca474bbb1fc783d
Dmit <dzhelezov@gmail.com>
2021-03-29 19:37:28 +0300

    feat(sample): add sample hooks and context-based mappings
    affects: sample, sample-mappings

--- packages/sample/mappings/mappings.ts
+++ packages/sample/mappings/mappings.ts
@@ -6,42 +6,72 @@ import { BlockTimestamp } from '../generated/graphql-server/src/modules/block-ti
 // to genenerate typescript classes for events, such as Balances.TransferEvent
 import { Balances, Timestamp } from './generated/types'
 import BN from 'bn.js'
+import { SubstrateEvent } from '@dzlzv/hydra-common'
 
 const start = Date.now()
-let total = 0
+let blockTime = 0
+let blockStartTime = 0
+let totalEvents = 0
+let totalBlocks = 0
 
-export async function balancesTransfer(
-  db: DatabaseManager,
-  event: Balances.TransferEvent
-) {
+export async function balancesTransfer({
+  store,
+  event,
+  block,
+}: {
+  store: DatabaseManager
+  event: SubstrateEvent
+  block: { blockNumber: number }
+}) {
   const transfer = new Transfer()
-  transfer.from = Buffer.from(event.data.accountIds[0].toHex())
-  transfer.to = Buffer.from(event.data.accountIds[1].toHex())
-  transfer.value = event.data.balance.toBn()
-  transfer.block = event.ctx.blockNumber
+  const _event = new Balances.TransferEvent(event)
+  transfer.from = Buffer.from(_event.data.accountIds[0].toHex())
+  transfer.to = Buffer.from(_event.data.accountIds[1].toHex())
+  transfer.value = _event.data.balance.toBn()
+  transfer.block = block.blockNumber
   transfer.comment = `Transferred ${transfer.value} from ${transfer.from} to ${transfer.to}`
   transfer.insertedAt = new Date()
-  await db.save<Transfer>(transfer)
+  await store.save<Transfer>(transfer)
 }
 
-export async function timestampCall(
-  db: DatabaseManager,
-  call: Timestamp.SetCall
-) {
-  benchmarkExtrinsics()
+export async function timestampCall({
+  store,
+  event,
+}: {
+  store: DatabaseManager
+  event: SubstrateEvent
+}) {
+  const call = new Timestamp.SetCall(event)
   const block = new BlockTimestamp()
   block.timestamp = call.args.now.toBn()
   block.blockNumber = new BN(call.ctx.blockNumber)
-  console.log(`New block ${block.blockNumber} at ${block.timestamp}`)
 
-  await db.save<BlockTimestamp>(block)
+  await store.save<BlockTimestamp>(block)
 }
 
-function benchmarkExtrinsics() {
+export async function preHook({
+  block: { eventCtxs },
+}: {
+  block: { eventCtxs: { event: SubstrateEvent }[] }
+}) {
+  blockStartTime = Date.now()
+  totalBlocks++
+  totalEvents += eventCtxs.length
+}
+
+export async function postHook() {
+  blockTime += Date.now() - blockStartTime
+  benchmark()
+}
+
+function benchmark() {
   const millis = Date.now() - start
-  total = total + 1
-  if (total % 10 === 0) {
+  if (totalEvents % 10 === 0) {
     console.log(`seconds elapsed = ${Math.floor(millis / 1000)}`)
-    console.log(`Everage time ms: ${millis / total}, total events: ${total}`)
+    console.log(
+      `Everage event time ms: ${millis / totalEvents}, block exec time ms: ${
+        blockTime / totalBlocks
+      }, total events: ${totalEvents}, total blocks: ${totalBlocks}`
+    )
   }
 }
