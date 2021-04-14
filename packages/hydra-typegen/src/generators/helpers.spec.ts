04da720d6de7843bae2471a18a1dd548d71c030b
Dmit <dzhelezov@gmail.com>
2021-04-14 19:20:32 +0300

    feat(hydra-typegen): use tuple-based return type for event parameters (vs type-based names)
    affects: @dzlzv/hydra-typegen

--- packages/hydra-typegen/src/generators/helpers.spec.ts
+++ packages/hydra-typegen/src/generators/helpers.spec.ts
@@ -1,10 +1,26 @@
 import { expect } from 'chai'
-import { nameFromType } from '.'
+import { helper } from './helpers'
+import { compact as c } from '../util'
 
 describe('helpers', () => {
-  it('should name from arg type', () => {
-    expect(nameFromType('Compact<Balance> & Codec')).to.equal('balance')
-    expect(nameFromType('MyLongType & Codec')).to.equal('myLongType')
-    expect(nameFromType('SimpleType')).to.equal('simpleType')
+  it('should render return type ', () => {
+    const prmsReturn = helper.paramsReturnType.bind({
+      args: ['Type1', 'Type2 & Codec', 'Option<Type3>'],
+    })
+    expect(c(prmsReturn())).to.equal(
+      c(`[Type1, Type2 & Codec, Option<Type3>]`)
+    )
+  })
+
+  it('should render return type statement', () => {
+    const prmsReturnStmt = helper.paramsReturnStmt.bind({
+      args: ['Type1', 'Type2 & Balance'],
+    })
+    expect(c(prmsReturnStmt())).to.equal(
+      c(`return [createTypeUnsafe<Type1 & Codec>(
+            typeRegistry, 'Type1', [this.ctx.params[0].value]), 
+            createTypeUnsafe<Type2 & Balance & Codec>(
+            typeRegistry, 'Type2 & Balance', [this.ctx.params[1].value])]`)
+    )
   })
 })
