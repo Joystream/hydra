9e86a8152df392d65f6e8092b4ddc4b9608b1ce9
denis <d.kezin@wintex.ru>
2021-12-22 14:25:57 +0700

    feat: hydra-cli: update version image'
    affects: @subsquid/hydra-cli

--- packages/hydra-cli/src/commands/squid/update.ts
+++ packages/hydra-cli/src/commands/squid/update.ts
@@ -1,5 +1,5 @@
 import { Command, flags } from '@oclif/command'
-import { release } from '../../rest-client/routes/release'
+import { update } from '../../rest-client/routes/update'
 import Debug from 'debug'
 import simpleGit, { SimpleGit, SimpleGitOptions } from 'simple-git'
 import {
@@ -8,15 +8,15 @@ import {
   parseNameAndVersion,
 } from '../../utils/helper'
 
-const debug = Debug('qnode-cli:deploy')
+const debug = Debug('qnode-cli:update')
 const options: Partial<SimpleGitOptions> = {
   baseDir: process.cwd(),
   binary: 'git',
 }
 const git: SimpleGit = simpleGit(options)
 
-export default class Release extends Command {
-  static description = 'Create a version'
+export default class Update extends Command {
+  static description = 'Update a version image'
   static args = [
     {
       name: 'nameAndVersion',
@@ -31,17 +31,17 @@ export default class Release extends Command {
       description: 'source',
       required: false,
     }),
-    description: flags.string({
-      char: 'd',
-      description: 'description',
+    hardReset: flags.boolean({
+      char: 'r',
+      description: 'perform a hard reset (db wipeout)',
       required: false,
+      default: false,
     }),
   }
 
   async run(): Promise<void> {
-    const { flags, args } = this.parse(Release)
+    const { flags, args } = this.parse(Update)
     debug(`Parsed flags: ${JSON.stringify(flags, null, 2)}, args: ${args}`)
-    const description = flags.description
     const nameAndVersion = args.nameAndVersion
     const { squidName, versionName } = parseNameAndVersion(nameAndVersion, this)
     let deployUrl = flags.source
@@ -55,7 +55,12 @@ export default class Release extends Command {
           }`
     }
     this.log(`🦑 Releasing the Squid at ${deployUrl}`)
-    const result = await release(squidName, versionName, deployUrl, description)
+    const result = await update(
+      squidName,
+      versionName,
+      deployUrl,
+      flags.hardReset
+    )
     this.log(
       '◷ You can detach from the resulting build process by pressing Ctrl + C. This does not cancel the deploy.'
     )
@@ -65,7 +70,7 @@ export default class Release extends Command {
     await pollDeployPipelines(
       squidName,
       versionName,
-      result?.version.deploymentUrl || '',
+      result?.deploymentUrl || '',
       this
     )
     this.log('✔️ Done!')
