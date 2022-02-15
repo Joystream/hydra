551402554631157755f5f3650fd5b7146fca5388
Igor Simonov <bart@chatfuel.com>
2022-02-15 17:52:38 +0300

    fix regex

--- scripts/livnesprobe.sh
+++ scripts/livnesprobe.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [[ ! -e old_head.txt  ]]; then 
-  curl localhost:9090/metrics | grep sqd_archive_chain_last_finalized_height | awk '{print $2}' > old_head.txt
+  curl localhost:9090/metrics | grep '^sqd_archive_chain_last_finalized_height' | awk '{print $2}' > old_head.txt
   result=1
 else
-  current=$(curl localhost:9090/metrics | grep sqd_archive_chain_last_finalized_height | awk '{print $2}')
+  current=$(curl localhost:9090/metrics | grep '^sqd_archive_chain_last_finalized_height' | awk '{print $2}')
   result=$current-$(cat $CHAIN.txt)
   echo "$current" > old_head.txt
 fi
